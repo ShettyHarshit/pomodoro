e94b1b0653d79694f4ccbc70304807cea02e8581
Harshit Shetty <swaharsh@gmail.com>
2021-10-03 01:18:35 +0530

    [illustration]: add coding illustration with subtext

--- src/components/IllustrationSidebar.js
+++ src/components/IllustrationSidebar.js
@@ -2,5 +2,16 @@ import React from "react";
 import "./styles.css";
 
 export default function IllustrationSidebar() {
-  return <div className="illustration-column">Yo</div>;
+  return (
+    <div className="illustration-column">
+      <div className="illustration-content">
+        <img
+          className="work-illustration"
+          src={require("../illustrations/coding.svg").default}
+          alt="illustration"
+        />
+        <h1>Plan Your Work And Stay Productive</h1>
+      </div>
+    </div>
+  );
 }
